5c7ae74a30f0023ea76d66233e24877f2d1948fb
saeid721 <saeid721@yahoo.com>
2023-08-05 16:47:44 -0700

    change main file & add two new file.

--- variables.dart
+++ variables.dart
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 void main(){
   int a = 6; //integer variable
   double b = 7.0; //Floating variable
@@ -12,4 +13,18 @@ void main(){
 
   bool dinnerDone = true; //Boolean variable
   print(dinnerDone);
+=======
+void main(){
+  int a = 6; //integer variable
+  double b = 7.0; //Floating variable
+  print(a+b);
+
+  String firstname = "Mr."; //String variable
+  String lastName = "Saeid"; //String variable
+  String fullName = firstname  + " " + lastName;
+  print(fullName);
+
+  bool dinnerDone = true; //Boolean variable
+  print(dinnerDone);
+>>>>>>> e896a78 (change main file & add two new file.)
 }
