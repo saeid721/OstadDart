66a5608b5785704d0e7340dac874c10f1a932946
Minhazul islam saeiD <saeid721@yahoo.com>
2023-08-06 04:11:43 -0700

    add new five file

---
+++ if_else.dart
@@ -0,0 +1,42 @@
+
+//this program for exam result using if, else if, & else statement
+
+void main(){
+  int marks = 110;
+/*
+  if (marks>=80 && marks<=100){
+    print("The result is A+");
+  } else if (marks>=70 && marks<80){
+    print("The result is A");
+  } else if (marks>=60 && marks<70){
+    print("Thee result is A-");
+  } else if (marks>=50 && marks<60){
+    print("The result is B");
+  } else if (marks>=40 && marks<50){
+    print("The result is C");
+  }else if (marks>=33 && marks<40){
+    print("The result is D");
+  } else{
+    print("The result is F");
+  }
+*/
+
+  if (marks<=80 && marks<=100){
+    print("The result is A+");
+  } else if (marks<80 && marks>=70){
+    print("The result is A");
+  } else if (marks<70 && marks>=60){
+    print("Thee result is A-");
+  } else if (marks<60 && marks>=50){
+    print("The result is B");
+  } else if (marks<50 && marks>=40){
+    print("The result is C");
+  }else if (marks<40 && marks>=33){
+    print("The result is D");
+  } else if (marks>100){
+    print("You type the invalid value, Please type the valid value");
+  }
+  else{
+    print("The result is F");
+  }
+}
---
+++ variables.dart
@@ -0,0 +1,15 @@
+void main(){
+  int a = 6; //integer variable
+  double b = 7.0; //Floating variable
+  print(a+b);
+
+  String firstname = "Mr."; //String variable
+  String lastName = "Saeid"; //String variable
+  String fullName = firstname  + " " + lastName; //String variable
+  String details = 'Full Name:' + " " + fullName;
+  print(fullName);
+  print(details);
+
+  bool dinnerDone = true; //Boolean variable
+  print(dinnerDone);
+}
