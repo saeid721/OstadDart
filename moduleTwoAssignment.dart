8f4e554a4d0e64ca371c34eb28ce6c0afe189e28
Minhazul islam saeiD <saeid721@yahoo.com>
2023-08-12 14:04:53 +0600

    Update moduleTwoAssignment.dart
    update

--- moduleTwoAssignment.dart
+++ moduleTwoAssignment.dart
@@ -1,3 +1,15 @@
+// মডিউল ২ এর এসাইনমেন্ট
+// You are given a list of integers representing the grades obtained by a student in
+// various subjects: [85, 92, 78, 65, 88, 72].
+// Write a Dart program to calculate the average grade of the student. After calculating
+// the average, round it to the nearest integer. Determine if the student's average grade
+// is greater than or equal to 70 print Passed. Print Failed otherwise
+//
+// Sample Output
+// Student's average grade: 80.0
+// Rounded average: 80
+// Passed
+
 import 'dart:io';
 
 void main() {
