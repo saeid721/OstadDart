4d96e9f0211ba563628f317bc4280a2a79024438
Minhazul islam saeiD <saeid721@yahoo.com>
2023-08-26 17:16:38 +0600

    update moduleFourAssignment

--- moduleFourAssignment.dart
+++ moduleFourAssignment.dart
@@ -1,3 +1,30 @@
+/*
+Define an abstract class Account with the following properties:
+●       accountNumber (integer)
+●       balance (double)
+
+It should also have the following methods:
+●       deposit(double amount): Adds the specified amount to the account balance.
+●       withdraw(double amount): Abstract method that deducts the specified amount from the account balance.
+
+Define a class SavingsAccount that extends the Account class. It should have an additional property called interestRate (double).
+Implement the withdraw() method in the SavingsAccount class as follows:
+●       Subtract the specified amount from the account balance.
+●       Apply the interest rate to the remaining balance.
+Define a class CurrentAccount that extends the Account class. It should have an additional property called overdraftLimit (double).
+
+Implement the withdraw() method in the CurrentAccount class as follows:
+
+●       Allow withdrawals up to the overdraft limit.
+●       If the withdrawal amount exceeds the overdraft limit, print a message indicating insufficient funds.
+In main()
+●       Create an instance of the SavingsAccount class by providing values for the account number, initial balance, and interest rate.
+●       Use the instance to perform operations like depositing and withdrawing money.
+●       Create an instance of the CurrentAccount class by providing values for the account number, initial balance, and overdraft limit.
+●       Use the instance to perform operations like depositing and withdrawing.
+
+ */
+
 abstract class Account {
   int accountNumber;
   double balance;
